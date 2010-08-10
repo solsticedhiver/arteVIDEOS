4c1a0f19bc1e17c0aee96d82dcdd19ef0323cff6
solsTiCe d'Hiver <solstice.dhiver@gmail.com>
2010-08-10 10:59:04 +0200

    Add a search command and a new command line interpreter
    * some minor refactoring: a function for each command
    * the cli is called when no option is given

--- arte+7.py
+++ arte+7.py
@@ -8,8 +8,108 @@ from os.path import exists as os_path_exists
 from os import environ as os_environ
 from sys import exit, argv, stderr
 from optparse import OptionParser
+#from tempfile import mkstemp
+from cmd import Cmd
 
+VERSION = '0.2'
+DEFAULT_LANG = 'fr'
+DEFAULT_QUALITY = 'hd'
 CLSID = 'clsid:d27cdb6e-ae6d-11cf-96b8-444553540000'
+SEARCH_URL = 'http://videos.arte.tv/%s/do_search/videos/%s?q='
+SEARCH_LANG = {'fr': 'recherche', 'de':'suche'}
+
+BOLD   = '[1m'
+NC     = '[0m'    # no color
+
+class ArgError(Exception):
+    pass
+
+class MyCmd(Cmd):
+    def __init__(self, videos, options):
+        Cmd.__init__(self)
+        self.prompt = 'arte+7> '
+        self.intro = '\nType "help" to see available commands.'
+
+        self.videos = videos
+        self.options = options
+
+    def process_num(self, arg):
+        num = int(arg)-1
+        if num < 0 or num >= len(self.videos):
+            raise ArgError
+
+        return 'http://videos.arte.tv'+self.videos[num].find('h2').a['href']
+
+    def do_url(self, arg):
+        '''url NUMBER
+    show the url of the chosen video'''
+        try:
+            url_page = self.process_num(arg)
+            print get_rtmp_url(url_page)[0]
+        except ValueError:
+            print 'Error: wrong argument (must be an integer)'
+        except ArgError:
+            print 'Error: no video with this number'
+
+    def do_play(self, arg):
+        '''play NUMBER
+    play the chosen video'''
+        try:
+            url_page = self.process_num(arg)
+            #video_url = get_rtmp_url(url_page)[0]
+            video_url = None
+            play(video_url)
+        except ValueError:
+            print 'Error: wrong argument (must be an integer)'
+        except ArgError:
+            print 'Error: no video with this number'
+
+    def do_record(self, arg):
+        '''record NUMBER
+    record the chosen video to a local file'''
+        try:
+            url_page = self.process_num(arg)
+            record(url_page, self.options)
+        except ValueError:
+            print 'Error: wrong argument (must be an integer)'
+        except ArgError:
+            print 'Error: no video with this number'
+
+    def do_search(self, arg):
+        '''search STRING
+    search for a given STRING on arte+7 web site'''
+        results = search(arg, self.options.lang)
+        if results is not None:
+            print_results(results)
+            self.videos = results
+
+    def do_help(self, arg):
+        '''print the help'''
+        if arg == '':
+            print '''COMMANDS:
+    url NUMBER      show real url of video
+    play NUMBER     play chosen video (NOT IMPLEMENTED YET)
+    record NUMBER   download and save video to a local file
+    search STRING   search for a video
+    help            show this help
+    quit            quit the cli
+    exit            exit the cli'''
+        else:
+            try:
+                print getattr(self, 'do_'+arg).__doc__
+            except AttributeError:
+                print >> stderr, 'Error: no help for command %s' % arg
+
+    def do_quit(self, arg):
+        '''quit the command line interpreter'''
+        return True
+
+    def do_exit(self, arg):
+        '''exit the command line interpreter'''
+        return True
+
+    def default(self, arg):
+        print 'Error: don\'t know how to %s' % arg
 
 def die(msg):
     print >> stderr, 'Error: %s. See %s --help' % (msg, argv[0])
@@ -49,19 +149,77 @@ def find_in_path(path, filename):
             return True
     return False
 
+def search(s, lang):
+    try:
+        url = (SEARCH_URL % (lang, SEARCH_LANG[lang])) + s.replace(' ', '+')
+        soup = BeautifulSoup(urlopen(url).read(), convertEntities=BeautifulStoneSoup.HTML_ENTITIES)
+        videos = soup.findAll('div', {'class': 'video'})
+
+        #handle, tmpname = mkstemp(prefix='arte+7-', suffix='.txt')
+        #tmp = open(tmpname, 'w')
+        #tmp.write('\n'.join('http://videos.arte.tv'+v.find('h2').a['href'] for v in videos))
+        #tmp.close()
+
+        return videos
+    except URLError:
+        die("Can't complete the requested search")
+    return None
+
+def print_results(results, verbose=True):
+    count = 0
+    for r in results:
+        count += 1
+        print BOLD + '(%d) '% count + r.find('h2').a.string + NC
+        if verbose:
+            print '    '+r.find('p', {'class': 'teaserText'}).string
+
+def play(video_url):
+    print >> stderr, 'Error: <play> is not implemented yet. Use record and then your favorite player'
+
+def record(url_page, options):
+    video_url, player_url = get_rtmp_url(url_page, quality=options.quality, lang=options.lang)
+    output_file = urlparse(url_page).path.split('/')[-1].replace('.html','.flv')
+    if not find_in_path(os_environ['PATH'], 'rtmpdump'):
+        print >> stderr, 'Error: rtmpdump has not been found'
+        exit(1)
+    cmd_args = '-r %s --swfVfy %s --flv %s' % (video_url, player_url, output_file)
+    if options.quiet:
+        cmd_args += ' --quiet'
+    if os_path_exists(output_file):
+        # try to resume a download
+        cmd_args += ' --resume'
+        print ':: Resuming download of %s' % output_file
+    else:
+        print ':: Downloading and saving to %s' % output_file
+
+    subprocess_call(['rtmpdump'] + cmd_args.split(' '))
+
 def main():
     usage = '''Usage: %prog url|play|record [OPTIONS] URL
-Play arte+7 videos without a mandatory browser or even record them. 
-You will need however to pass the url of the webpage presenting the video.
-They all begin with http://videos.arte.tv/fr/videos/{...}
-    url     just show the real url of the video
-    play    play the video directly (NOT IMPLEMENTED)
-    record  record the video to a file'''
+       %prog search [OPTIONS] STRING...
+
+Play or record arte+7 videos without a mandatory browser.
+
+You need to get the url of the page presenting the video on arte+7 site
+(so you might need a browser after all) ;-)
+or use the search command to get a list of videos
+
+COMMANDS
+    url     show the real url of the video
+    play    play the video directly (NOT IMPLEMENTED YET)
+    record  save the video into a local file
+    search  search for a video on arte+7
+            It will display a numbered list of results and enter
+            a simple command line interpreter that has the same 4 commands:
+                url NUMBER
+                play NUMBER (NOT IMPLEMENTED YET)
+                record NUMBER
+                search STRING'''
 
     parser = OptionParser(usage=usage)
-    parser.add_option('-l', '--lang', dest='lang', type='string', default='fr',
+    parser.add_option('-l', '--lang', dest='lang', type='string', default=DEFAULT_LANG,
             action='store', help='language of the video fr or de (default: fr)')
-    parser.add_option('-q', '--quality', dest='quality', type='string', default='hd',
+    parser.add_option('-q', '--quality', dest='quality', type='string', default=DEFAULT_QUALITY,
             action='store', help='quality of the video sd or hd (default: hd)')
     parser.add_option('--quiet', dest='quiet', default=False,
             action='store_true', help='do not show output of rtmpdump')
@@ -72,35 +230,29 @@ They all begin with http://videos.arte.tv/fr/videos/{...}
         die('Invalid option')
     if options.quality not in ('sd', 'hd'): # what is EQ ?
         die('Invalid option')
-    if len(args) != 2 or args[0] not in ('url', 'play', 'record'):
+    if len(args) < 2:
+        MyCmd([], options).cmdloop()
+        exit(0)
+    if args[0] not in ('url', 'play', 'record', 'search'):
         die('Invalid command')
 
     if args[0] == 'play':
-        print >> stderr, 'Error: <play> is not implemented yet. Use record and then your favorite player'
+        # video_url = get_rtmp_url(args[1], quality=options.quality, lang=options.lang)[0]
+        video_url = None
+        play(video_url)
         exit(1)
 
-    url_page = args[1]
-    video_url, player_url = get_rtmp_url(url_page, quality=options.quality, lang=options.lang)
-    output_file = urlparse(url_page).path.split('/')[-1].replace('.html','.flv')
+    elif args[0] == 'url':
+        print get_rtmp_url(args[1], quality=options.quality, lang=options.lang)[0]
 
-    if args[0] == 'url':
-        print video_url
-        exit(0)
     elif args[0] == 'record':
-        if not find_in_path(os_environ['PATH'], 'rtmpdump'):
-            print >> stderr, 'Error: rtmpdump has not been found'
-            exit(1)
-        cmd_args = '-r %s --swfVfy %s --flv %s' % (video_url, player_url, output_file)
-        if options.quiet:
-            cmd_args += ' --quiet'
-        if os_path_exists(output_file):
-            # try to resume a download
-            cmd_args += ' --resume'
-            print ':: Resuming download of %s' % output_file
-        else:
-            print ':: Downloading and saving to %s' % output_file
+        record(args[1], options)
 
-        subprocess_call(['rtmpdump'] + cmd_args.split(' '))
+    elif args[0] == 'search':
+        results = search(' '.join(args[1:]), options.lang)
+        if results is not None:
+            print_results(results)
+            MyCmd(results, options).cmdloop()
 
 if __name__ == '__main__':
     try:
